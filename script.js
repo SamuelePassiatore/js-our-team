87729a9839f8e8b4442bda8bc6567a20a5ebc57d
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-21 17:20:38 +0100

    added card & contents to html

--- script.js
+++ script.js
@@ -6,53 +6,58 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 */
 
 // Prendo gli elementi dal DOM
-const test = document.getElementById('test');
+const row = document.querySelector('.row');
 
 // Crea un array di oggetti per rappresentare i membri del team
 const team = [
     {
       name: 'Wayne Barnett',
       role: 'Founder & CEO',
-      'url': 'img/wayne-barnett-founder-ceo.jpg'
+      image: 'img/wayne-barnett-founder-ceo.jpg'
     },
     {
       name: 'Angela Caroll',
       role: 'Chief Editor',
-      'url': 'img/angela-caroll-chief-editor.jpg'
+      image: 'img/angela-caroll-chief-editor.jpg'
     },
     {
       name: 'Walter Gordon',
       role: 'Office Manager',
-      'url': 'img/walter-gordon-office-manager.jpg'
+      image: 'img/walter-gordon-office-manager.jpg'
     },
     {
       name: 'Angela Lopez',
       role: 'Social Media Manager',
-      'url': 'img/angela-lopez-social-media-manager.jpg'
+      image: 'img/angela-lopez-social-media-manager.jpg'
     },
     {
       name: 'Scott Estrada',
       role: 'Developer',
-      'url': 'img/scott-estrada-developer.jpg'
+      image: 'img/scott-estrada-developer.jpg'
     },
     {
       name: 'Barbara Ramos',
       role: 'Graphic Designer',
-      'url': 'img/barbara-ramos-graphic-designer.jpg'
+      image: 'img/barbara-ramos-graphic-designer.jpg'
     }
   ];
 
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+let content = '';
 for (let i = 0; i < team.length; i++){
     const currentMember = team[i];
     console.log(currentMember);
-
-  let member = '<div>';
-  test.innerText += `
-  <h3>${team[i].name}</h3>
-  <p>${team[i].role}</p>
-  <img src="${team[i].url}" alt="Foto di ${team[i].name}">
+  content += `
+  <div class="col-4 my-4">
+    <div class="card h-100 border-0 text-center">
+        <img src="${team[i].image}" alt="Foto di ${team[i].name} class="card-img-top rounded-0">
+        <h5 class="card-title">${team[i].name}</h5>
+        <span class="card-text">${team[i].role}</span>
+    </div>
+  </div>
   `
-  member = '</div>';
 }
+  row.innerHTML = content;
+
+
 
