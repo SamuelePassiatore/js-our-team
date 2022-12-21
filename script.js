288fbc831f5c747b5c98ee95522053bcda9ef27a
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-21 15:28:02 +0100

    create array team members

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membr
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 */
 
+// Crea un array di oggetti per rappresentare i membri del team
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      'url': 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      'url': 'img/angela-caroll-chief-editor.jpg'
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      'url': 'img/walter-gordon-office-manager.jpg'
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      'url': 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      'url': 'img/scott-estrada-developer.jpg'
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      'url': 'img/barbara-ramos-graphic-designer.jpg'
+    }
+  ];
