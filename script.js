8d47aa22db5118b30040801c173b8ea3b9cdcbbd
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-22 10:41:30 +0100

    general changes

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membr
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 */
 
+// ! OPERAZIONI PRELIMINARI
 // Prendo gli elementi dal DOM
-const row = document.querySelector('.row');
+const rowTeam = document.querySelector('.row');
 
+// ! SVOLGIMENTO
 // Crea un array di oggetti per rappresentare i membri del team
-const team = [
+const teamMembers = [
     {
       name: 'Wayne Barnett',
       role: 'Founder & CEO',
@@ -43,19 +45,26 @@ const team = [
   ];
 
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-let currentMember = '';
-for (let i = 0; i < team.length; i++){
-  currentMember += `
+let content = '';
+for (let i = 0; i < teamMembers.length; i++){
+  const currentMember = teamMembers[i];
+  // Ciclo for in per stampare in console
+  for (let key in currentMember){
+    ////console.log(currentMember[key]);
+  }
+  // Inserisco contenuti nel DOM
+  content += `
   <div class="col-xl-4 col-lg-6 col-sm-12 my-4">
     <div class="card h-100 border-0 text-center">
-        <img src="${team[i].image}" alt="Foto di ${team[i].name} class="card-img-top rounded-0">
-        <h5 class="card-title pt-3">${team[i].name}</h5>
-        <span class="card-text pb-3">${team[i].role}</span>
+        <img src="${currentMember.image}" alt="${currentMember.name} class="card-img-top rounded-0">
+        <h5 class="card-title pt-3">${currentMember.name}</h5>
+        <span class="card-text pb-3">${currentMember.role}</span>
     </div>
   </div>
-  `
+  `;
 }
-  row.innerHTML = currentMember;
+// Stampo in pagina
+rowTeam.innerHTML = content;
 
 
 
